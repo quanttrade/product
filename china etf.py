b89ff32c9511cb5d81516edd25c30c00051b3855
caifengsteven <31440870+caifengsteven@users.noreply.github.com>
2022-06-24 06:48:02 +0800

    Update china etf.py

--- china etf.py
+++ china etf.py
@@ -24,3 +24,23 @@ for i in range(x-1):
     ret.append((df_return.loc[i+2,[col]].values[0]))
 print(ret)
 plt.plot(np.cumsum(ret))
+
+
+df = pd.read_excel('china_etf1.xlsx')
+print(df)
+df_return = df[['512000_return','512170_return','512200_return','512290_return','512400_return','512480_return','512690_return','512980_return','515210_return','515220_return','512800_return','512660_return']]
+df_return = df_return[1:]
+print(df_return)
+id_max = df_return.idxmax(axis=1)
+print(id_max)
+x = len(df_return)
+ret1 = []
+for i in range(x-1):
+    print(i)
+    col =id_max.iloc[i]
+    print(col)
+    ret1.append((df_return.loc[i+2,[col]].values[0]))
+print(ret1)
+ret_2 = np.array(ret1)-np.array(ret)
+plt.plot(np.cumsum(ret1))
+plt.plot(np.cumsum(ret_2))
